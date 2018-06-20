48c165f38476f4cd84ed0b3093ba95ae80a9d36f
dbuech <pib.dominic.buech@htwsaar.de>
2018-06-20 09:08:29 +0200

    Update
    Latest update

---
+++ chessengine/src/main/java/TimeManager.java
@@ -0,0 +1,12 @@
+/**
+ * Class to manage the remaining time and set the max time for finding a best move
+ */
+public class TimeManager {
+
+    private int playerTime;
+    private int engineTime;
+
+    public void setTurnTime(){
+
+    }
+}
