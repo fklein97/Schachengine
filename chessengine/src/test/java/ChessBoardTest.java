afc525f8f35d8e0bf8983bea544e5eb3ca4779b8
TheFK <35765059+TheFK@users.noreply.github.com>
2018-06-20 13:01:17 +0200

    fixed pom file and added some Imports

--- chessengine/src/test/java/ChessBoardTest.java
+++ chessengine/src/test/java/ChessBoardTest.java
@@ -1,7 +1,7 @@
-//import org.junit.Test;
+import org.junit.*;
 import org.junit.jupiter.api.Test;
 import java.util.*;
-
+import Board.*;
 //import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
--- chessengine/src/test/java/MoveGeneratorTest.java
+++ chessengine/src/test/java/MoveGeneratorTest.java
@@ -1,5 +1,6 @@
-import org.junit.jupiter.api.Test;
+import org.junit.*;
 import java.util.ArrayList;
+import Board.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
