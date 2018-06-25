b77fff807e65e96924c9deb97e35154aa501e97c
TheFK <35765059+TheFK@users.noreply.github.com>
2018-06-25 22:01:58 +0200

    fixed MoveGenerator merge bugs (github why again?)

--- chessengine/src/test/java/MoveGeneratorTest.java
+++ chessengine/src/test/java/MoveGeneratorTest.java
@@ -34,12 +34,10 @@ public class MoveGeneratorTest {
         expectedMoveset.add(new Position(4,3,whitepawn));
         expectedMoveset.add(new Position(4,4,whitepawn));
 
-<<<<<<< HEAD
        //Assert.assertEquals(expectedMoveset, MoveGenerator.getMoveSet(new Position(4,2,whitepawn)));
-=======
+
         Assert.assertEquals(expectedMoveset.get(0).equals(calculatedMoveset.get(0)) , true);
         Assert.assertEquals(expectedMoveset.get(1).equals(calculatedMoveset.get(1)) , true);
->>>>>>> master
     }
 
 
